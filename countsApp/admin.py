b630c934f83b1756d05ad338463376e5d6a08114
Reynaldo Gil García <gilcu2@gmail.com>
2013-08-01 16:22:09 +0200

    First Step

---
+++ countsApp/admin.py
@@ -0,0 +1,18 @@
+__author__ = 'gil'
+
+from django.contrib import admin
+from countsApp.models import Count,User
+
+class CountInline(admin.StackedInline):
+    model = Count
+    extra = 3
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                   {'fields': ['name']}),
+        ('Counts',               {'fields': ['email'],'classes': ['collapse']}),
+
+    ]
+    inlines = [CountInline]
+
+admin.site.register(User,UserAdmin)
--- countsApp/models.py
+++ countsApp/models.py
@@ -14,13 +14,14 @@ class User(models.Model):
         return self.name+'-'+self.email
 
 class Count(models.Model):
+    user=models.ForeignKey(User)
     name = models.CharField(max_length=20)
     count=models.IntegerField(default=0)
     last_update = models.DateTimeField()
-    user_id=models.ForeignKey(User)
+
 
     def __unicode__(self):
-        return self.user_id.name+'.'+self.name+':'+str(self.count)+' ('+str(self.last_update)+')'
+        return self.user.name+'.'+self.name+':'+str(self.count)+' ('+str(self.last_update)+')'
 
     def recently_updated(self):
         return self.last_update >= timezone.now() - datetime.timedelta(days=7)
