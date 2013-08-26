7cb73f150586f459f15e18ca6d60c808ac6549fc
Reynaldo Gil García <gilcu2@gmail.com>
2013-08-26 10:01:23 +0200

    index counts view

--- countsApp/views.py
+++ countsApp/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from countsApp.models import Count
 
 def index(request):
     """
@@ -8,4 +11,12 @@ def index(request):
     :param request:
     :return:
     """
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_count_list = Count.objects.order_by('-last_update')[:5]
+    template = loader.get_template('countsApp/index.html')
+    context = RequestContext(request, {
+        'latest_count_list': latest_count_list,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, count_id):
+    return HttpResponse("You're looking at counts %s." % count_id)
